f0df513b6f2c4db4cb13ad6e14bb3a42be7be69f
shuaiquan <994011557@qq.com>
2021-12-21 09:57:34 +0800

    初始化模型实体事件管理器

--- src/entity/event-emitter/index.ts
+++ src/entity/event-emitter/index.ts
@@ -1,18 +1,94 @@
+import { InteractiveType } from "../../listener";
+
+type Listener = (...arg: any[]) => void;
+
+class EventObject {
+    fn: Listener;
+    once: boolean;
+
+    constructor(fn: Listener, once: boolean) {
+        this.fn = fn;
+        this.once = once;
+    }
+}
+
 /**
- * TODO 考虑一下模块的接口设计以及功能涵盖范围
+ * 模型实体事件管理器
  */
 class EntityEmitter {
-    // on() {
-    //     // todo
-    // }
+    private eventMap: Map<InteractiveType, EventObject[]> = new Map();
 
-    // remove() {
-    //     // todo
-    // }
+    private addEventListener(type: InteractiveType, listener: Listener, once: boolean) {
+        if (!(listener instanceof Function)) {
+            throw new Error('listener must be a function');
+        }
 
-    // emit() {
-    //     // todo
-    // }
+        if (!this.eventMap.has(type)) {
+            this.eventMap.set(type, []);
+        }
+
+        const listeners = this.eventMap.get(type);
+        listeners.push(new EventObject(listener, once));
+    }
+
+    private removeEventListener(type: InteractiveType, listener?: Listener) {
+        if (!this.eventMap.has(type)) {
+            return this;
+        }
+
+        if (!listener) {
+            this.eventMap.delete(type);
+            return this;
+        }
+
+        const listeners = this.eventMap.get(type);
+        for (let i = 0; i < listeners.length;) {
+            const obj = listeners[i];
+            if (obj.fn === listener) {
+                listeners.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+    }
+
+    on(type: InteractiveType, listener: Listener) {
+        this.addEventListener(type, listener, false);
+        return this;
+    }
+
+    once(type: InteractiveType, listener: Listener) {
+        this.addEventListener(type, listener, true);
+        return this;
+    }
+
+    off(type: InteractiveType, listener: Listener) {
+        this.removeEventListener(type, listener);
+    }
+
+    offAll(type: InteractiveType) {
+        this.removeEventListener(type);
+    }
+
+    emit(type: InteractiveType) {
+        if (!this.eventMap.has(type)) {
+            return this;
+        }
+
+        const listeners = this.eventMap.get(type);
+        for (let i = 0; i < listeners.length;) {
+            const obj = listeners[i];
+
+            obj.fn();
+
+            if (obj.once) {
+                listeners.splice(i, 1);
+            } else {
+                i++;
+            }
+        }
+        return this;
+    }
 }
 
 export {
