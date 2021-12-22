13b3f12afaccccfb4214a49811511a0850e83f5c
shuaiquan <994011557@qq.com>
2021-12-22 09:22:58 +0800

    EntityEmitter 功能说明

--- src/entity/event-emitter/index.ts
+++ src/entity/event-emitter/index.ts
@@ -16,9 +16,18 @@ class EventObject {
  * 模型实体事件管理器
  */
 class EntityEmitter {
+    /**
+     * 交互事件对应处理函数
+     */
     private eventMap: Map<InteractiveType, EventObject[]> = new Map();
 
-    private addEventListener(type: InteractiveType, listener: Listener, once: boolean) {
+    /**
+     * 注册交互事件
+     * @param type 交互类型
+     * @param listener 回调函数
+     * @param once 是否只触发一次（默认：false）
+     */
+    private addEventListener(type: InteractiveType, listener: Listener, once: boolean = false) {
         if (!(listener instanceof Function)) {
             throw new Error('listener must be a function');
         }
@@ -31,6 +40,11 @@ class EntityEmitter {
         listeners.push(new EventObject(listener, once));
     }
 
+    /**
+     * 移除交互事件
+     * @param type 交互类型
+     * @param listener 回调函数（可选，不传时会取消该交互事件下的所有回调函数）
+     */
     private removeEventListener(type: InteractiveType, listener?: Listener) {
         if (!this.eventMap.has(type)) {
             return this;
@@ -52,24 +66,50 @@ class EntityEmitter {
         }
     }
 
+    /**
+     * 注册交互事件监听
+     * @param type 交互类型
+     * @param listener 回调函数
+     */
     on(type: InteractiveType, listener: Listener) {
         this.addEventListener(type, listener, false);
         return this;
     }
 
+    /**
+     * 注册一次性的交互事件监听
+     * @param type 交互类型
+     * @param listener 回调函数
+     * @returns 
+     */
     once(type: InteractiveType, listener: Listener) {
         this.addEventListener(type, listener, true);
         return this;
     }
 
+    /**
+     * 移除交互事件监听
+     * @param type 交互类型
+     * @param listener 回调函数
+     */
     off(type: InteractiveType, listener: Listener) {
         this.removeEventListener(type, listener);
+        return this;
     }
 
+    /**
+     * 移除该交互类型的全部监听事件
+     * @param type 交互类型
+     */
     offAll(type: InteractiveType) {
         this.removeEventListener(type);
+        return this;
     }
 
+    /**
+     * 触发交互事件
+     * @param type 交互类型
+     */
     emit(type: InteractiveType) {
         if (!this.eventMap.has(type)) {
             return this;
